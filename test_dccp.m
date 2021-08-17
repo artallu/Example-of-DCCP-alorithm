0c1edf2b8438b46f30acdf73023dd3115e7be51e
artallu <42709706+artallu@users.noreply.github.com>
2021-08-17 10:32:44 +0800

    Add files via upload

---
+++ test_dccp.m
@@ -0,0 +1,37 @@
+clc
+clear all
+f=@(y) y.^2-0.6*y.^2;
+gb=@(y,yk) 0.6*yk^2+1.2*yk*(y-yk);
+% y=-10:10;
+% ykv=-20:0;
+% for i=1:length(ykv)
+%     yk=ykv(i);
+% 
+% plot(y,gb(y,yk))
+% hold on
+% end
+y=-50:0.1:50;
+plot(y,f(y),'b')
+hold on
+[a,b]=min(f(y));
+val=y(b);
+
+xk=-50;
+
+for i=1:40
+    yk=xk;
+cvx_begin quiet
+variable x
+g=0.6*xk^2+1.2*xk*(x-xk);
+yk;
+plot(y,y.^2-gb(y,yk))
+hold on
+minimize x^2-g
+subject to
+x>=-30;
+x<=10;
+cvx_end
+scatter(x,cvx_optval)
+xk=x;
+end
+axis([-50 50 -10 1500])
